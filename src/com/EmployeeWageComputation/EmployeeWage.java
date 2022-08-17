ce925a250df11d4b1c2a25e765c9a13ca45bc035
Akshayw26 <wangade30@gmail.com>
2022-08-17 16:50:42 +0530

    Added - Class Method

--- src/com/EmployeeWageComputation/EmployeeWage.java
+++ src/com/EmployeeWageComputation/EmployeeWage.java
@@ -9,7 +9,8 @@ public class EmployeeWage {
     static final int IS_FULL_TIME = 2;
     static final int WORKING_DAYS_PER_MONTH = 20;
     static final int WORKING_HOURS_PER_MONTH = 100;
-    public static void main(String[] args) {
+
+    public static int calculateEmpWage(int WAGE_PER_HOUR, int WORKING_DAYS_PER_MONTH, int WORKING_HOURS_PER_MONTH) {
 
         System.out.println("Welcome to Employee Wage Computation Program");
         System.out.println("---------------------------------------------");
@@ -19,11 +20,11 @@ public class EmployeeWage {
         int dayCount = 1;
         int workingHours = 0;
 
-        while(dayCount <= WORKING_DAYS_PER_MONTH && workingHours <= WORKING_HOURS_PER_MONTH){
-
-            int empPresent = (int) Math.floor(Math.random() * 10) % 3;
+        while (dayCount <= WORKING_DAYS_PER_MONTH && workingHours <= WORKING_HOURS_PER_MONTH) {
 
             int empWage = 0;
+
+            int empPresent = (int) Math.floor(Math.random() * 10) % 3;
 
             switch (empPresent) {
                 case IS_PART_TIME:
@@ -41,13 +42,18 @@ public class EmployeeWage {
             }
 
             totatWage += empWage;
-            System.out.println("Employee Daily Wage Day " +dayCount+ " => " +empWage);
+            System.out.println("Employee Daily Wage Day " + dayCount + " => " + empWage);
             dayCount++;
 
         }
-
         System.out.println("--------------------------------------");
-        System.out.println("Total Working Hours => " +workingHours);
-        System.out.println("Total wage => " +totatWage);
+        System.out.println("Total Working Hours => " + workingHours);
+        System.out.println("Total wage => " + totatWage);
+        return totatWage;
     }
+    public static void main(String[] args) {
+
+        calculateEmpWage(20,20,75);
+    }
+
 }
