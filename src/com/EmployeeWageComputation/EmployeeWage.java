d40f8eddda5d3fddfa92a8b11f60ac6883fdb220
Akshayw26 <wangade30@gmail.com>
2022-08-17 16:34:14 +0530

    Added - Employee Attendance

--- src/com/EmployeeWageComputation/EmployeeWage.java
+++ src/com/EmployeeWageComputation/EmployeeWage.java
@@ -4,7 +4,14 @@ public class EmployeeWage {
     public static void main(String[] args) {
 
         System.out.println("Welcome To Employee Wage Computation");
+        System.out.println("---------------------------------------------");
 
+        int empPresent = (int)Math.floor(Math.random() * 10) % 2;
+
+          if(empPresent == 1)
+            System.out.println("Employee is Present");
+          else
+            System.out.println("Employee is Absent");
         
     }
 }
