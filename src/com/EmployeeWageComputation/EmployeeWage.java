2b18427d5abc14f1dbf775ce87044558bc97a943
Akshayw26 <wangade30@gmail.com>
2022-08-17 16:56:16 +0530

    Added - Wage For Multipal Companies

--- src/com/EmployeeWageComputation/EmployeeWage.java
+++ src/com/EmployeeWageComputation/EmployeeWage.java
@@ -1,59 +1,54 @@
 package com.EmployeeWageComputation;
 
 public class EmployeeWage {
-
-    static final int WAGE_PER_HOUR = 20;
-    static final int FULL_DAY_HOUR = 8;
-    static final int PART_TIME_HOUR = 4;
     static final int IS_PART_TIME = 1;
     static final int IS_FULL_TIME = 2;
-    static final int WORKING_DAYS_PER_MONTH = 20;
-    static final int WORKING_HOURS_PER_MONTH = 100;
 
-    public static int calculateEmpWage(int WAGE_PER_HOUR, int WORKING_DAYS_PER_MONTH, int WORKING_HOURS_PER_MONTH) {
+    public static int calculateEmpWage(String companyName, int empWagePerHour, int numOfWorkingDays,int maxHoursPerMonth) {
 
         System.out.println("Welcome to Employee Wage Computation Program");
         System.out.println("---------------------------------------------");
 
-        int totatWage = 0;
-        int totalWorkingHours = 0;
-        int dayCount = 1;
-        int workingHours = 0;
+        int empHours = 0;
+        int totalEmpHours = 0;
+        int totalWorkingDays = 0;
 
-        while (dayCount <= WORKING_DAYS_PER_MONTH && workingHours <= WORKING_HOURS_PER_MONTH) {
-
-            int empWage = 0;
+        while (totalEmpHours <= maxHoursPerMonth && totalWorkingDays < numOfWorkingDays) {
+            totalWorkingDays++;
 
             int empPresent = (int) Math.floor(Math.random() * 10) % 3;
 
             switch (empPresent) {
                 case IS_PART_TIME:
-                    empWage = WAGE_PER_HOUR * FULL_DAY_HOUR;
-                    System.out.println("Employee is Present as Full Time");
-                    workingHours += FULL_DAY_HOUR;
+                    System.out.println("Employee is present as part time");
+                    empHours = 4;
                     break;
                 case IS_FULL_TIME:
-                    System.out.println("Employee is Present as Part Time");
-                    empWage = WAGE_PER_HOUR * PART_TIME_HOUR;
-                    workingHours += PART_TIME_HOUR;
+                    System.out.println("Employee is present as full time");
+                    empHours = 8;
                     break;
                 default:
                     System.out.println("Employee is absent");
+                    empHours = 0;
+                    break;
             }
 
-            totatWage += empWage;
-            System.out.println("Employee Daily Wage Day " + dayCount + " => " + empWage);
-            dayCount++;
-
+            totalEmpHours += empHours;
+            System.out.println("Day: " +totalWorkingDays+ " Employee working hours : " +empHours);
         }
-        System.out.println("--------------------------------------");
-        System.out.println("Total Working Hours => " + workingHours);
-        System.out.println("Total wage => " + totatWage);
-        return totatWage;
+
+        int totalEmpWage = totalEmpHours * empWagePerHour;
+        System.out.println("Total Employee wage for company " +companyName+ " is " +totalEmpWage);
+        System.out.println("---------------------------------------------");
+        return totalEmpWage;
     }
     public static void main(String[] args) {
 
-        calculateEmpWage(20,20,75);
-    }
+        calculateEmpWage("jio",25,25,80);
 
+        calculateEmpWage("Tata",20,30,90);
+
+        calculateEmpWage("Reliance",25,20,80);
+
+    }
 }
